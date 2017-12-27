4e64e455df4f72339846201c65b98d154e0008ac
马三小伙儿 <1790617178@qq.com>
2017-12-28 00:10:29 +0800

    CustomLogHandler

---
+++ Assets/Scripts/CommonHelper/ColaFrameLogHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class ColaFrameLogHandler : ILogHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LogFormat(LogType logType, Object context, string format, params object[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogException(Exception exception, Object context)
+    {
+        throw new NotImplementedException();
+    }
+}
