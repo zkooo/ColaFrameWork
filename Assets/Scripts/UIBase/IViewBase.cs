723016f612f03f7ddd17731bfe7158d245b7ebb8
马三小伙儿 <1790617178@qq.com>
2017-12-31 23:42:24 +0800

    添加UI行为接口

--- Assets/Scripts/UIBase/IViewBase.cs
+++ Assets/Scripts/UIBase/IViewBase.cs
@@ -7,7 +7,47 @@ using UnityEngine;
 /// </summary>
 public interface IViewBase
 {
-    
+    /// <summary>
+    /// 打开一个UI界面
+    /// </summary>
+    void Open();
+
+    /// <summary>
+    /// 关闭一个UI界面
+    /// </summary>
+    void Close();
+
+    /// <summary>
+    /// UI界面显隐的时候会调用该方法
+    /// </summary>
+    /// <param name="isShow"></param>UI的是否显示
+    void OnShow(bool isShow);
+
+    /// <summary>
+    /// 创建UI界面
+    /// </summary>
+    void Create();
+
+    /// <summary>
+    /// UI创建过程中会调用该方法
+    /// </summary>
+    void OnCreate();
+
+    /// <summary>
+    /// 销毁一个UI界面
+    /// </summary>
+    void Destroy();
+
+    /// <summary>
+    /// 销毁UI界面的过程中调用该方法
+    /// </summary>
+    void OnDestroy();
+
+    /// <summary>
+    /// 设置一个UI界面的显隐
+    /// </summary>
+    /// <param name="isActive"></param>UI界面是否显示
+    void SetActive(bool isActive);
 }
 
 
