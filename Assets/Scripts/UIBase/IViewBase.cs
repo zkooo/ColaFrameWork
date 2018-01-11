122dc4804bf348d05edcedcdb7afe894b7a04309
马三小伙儿 <1790617178@qq.com>
2018-01-11 16:36:47 +0800

    添加UIManager接口

--- Assets/Scripts/UIBase/IViewBase.cs
+++ Assets/Scripts/UIBase/IViewBase.cs
@@ -10,6 +10,55 @@ public interface IViewBase
 
 }
 
+/// <summary>
+/// UI管理器接口
+/// </summary>
+public interface IViewManager
+{
+    /// <summary>
+    /// UI管理器，打开某种类型的UI
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    void Open<T>();
+
+    /// <summary>
+    /// UI管理器，关闭某种类型的UI
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    void Close<T>();
+
+    /// <summary>
+    /// UI管理器，销毁某种类型的UI
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    void Destroy<T>();
+
+    /// <summary>
+    /// UI管理器，刷新某种类型的UI
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="eventData"></param>
+    void UpdateUI<T>(EventData eventData);
+
+    /// <summary>
+    /// UI管理器，获取某种类型的UI
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    T GetViewByType<T>() where T:UIBase;
+
+    /// <summary>
+    /// UI管理器，将一个UI加入到UI管理器中参与管理
+    /// </summary>
+    /// <param name="ui"></param>
+    void AddView(UIBase ui);
+
+    /// <summary>
+    /// UI管理器，从UI管理器中移除某个UI，使其不再参与管理
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    void RemoveViewByType<T>() where T : UIBase;
+}
 
 /// <summary>
 /// UI的类型枚举
