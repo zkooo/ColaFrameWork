b875e05b405bd8530dae7e74f19956dd2da72ab9
马三小伙儿 <1790617178@qq.com>
2018-01-11 18:46:04 +0800

    UIManager管理器

--- Assets/Scripts/CommonMgr/UIMgr.cs
+++ Assets/Scripts/CommonMgr/UIMgr.cs
@@ -1,6 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIMgr : MonoBehaviour {
+public class UIMgr
+{
+    private static UIMgr instance;
+
+    /// <summary>
+    /// 存储UI控制脚本的字典
+    /// </summary>
+    private Dictionary<Type, UIBase> uiList;
+
+    private UIMgr()
+    {
+        uiList = new Dictionary<Type, UIBase>()
+        {
+            
+        };
+    }
+
+    public static UIMgr GetInstance()
+    {
+        if (null == instance)
+        {
+            instance = new UIMgr();
+        }
+        return instance;
+    }
 }
