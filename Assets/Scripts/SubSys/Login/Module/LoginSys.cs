a1bedd09afca1237384454c40475c960e5cc47a4
马三小伙儿 <1790617178@qq.com>
2018-01-21 23:26:36 +0800

    Login系统测试代码完善

--- Assets/Scripts/SubSys/Login/Module/LoginSys.cs
+++ Assets/Scripts/SubSys/Login/Module/LoginSys.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EventType = ColaFrame.EventType;
 
 public class LoginSys : SubSysBase {
     public LoginSys() : base(SubSysType.Login)
     {
     }
 
+    public override void EnterSys()
+    {
+        base.EnterSys();
+        GameEventMgr.GetInstance().DispatchEvent("uiLoginPanelOpen",EventType.UIMsg,new List<string>(){"测试数据"});
+    }
+
+    public override void ExitSys()
+    {
+        base.ExitSys();
+    }
+
     public override void UpdateSys(double deltaTime)
     {
         base.UpdateSys(deltaTime);
         Debug.LogWarning("登录系统刷新"+deltaTime);
+    }
+
+    protected override void RegisterHander()
+    {
+        base.RegisterHander();
     }
 }
--- Assets/Scripts/SubSys/Login/View/UILogin.cs
+++ Assets/Scripts/SubSys/Login/View/UILogin.cs
@@ -4,11 +4,52 @@ using UnityEngine;
 
 public class UILogin : UIBase
 {
-    protected UILogin(int resId, UIType uiType) : base(resId, uiType)
+    public UILogin(int resId, UIType uiType) : base(resId, uiType)
     {
     }
 
-    protected UILogin(GameObject panel, GameObject parent, UIType uiType) : base(panel, parent, uiType)
+    public UILogin(GameObject panel, GameObject parent, UIType uiType) : base(panel, parent, uiType)
     {
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
+
+    public override void Create()
+    {
+        base.Create();
+    }
+
+    public override void Destroy()
+    {
+        base.Destroy();
+    }
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+    }
+
+    public override void OnShow(bool isShow)
+    {
+        base.OnShow(isShow);
+    }
+
+    public override void Open()
+    {
+        base.Open();
+        Debug.LogWarning("UI登录系统打开");
+    }
+
+    public override void UpdateUI(EventData eventData)
+    {
+        base.UpdateUI(eventData);
     }
 }
