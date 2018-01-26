9fab5adbd9b76160b66d8d3919aed44d6a6b885c
马三小伙儿 <1790617178@qq.com>
2018-01-26 18:55:47 +0800

    图集打包管理接口编写

--- Assets/Editor/SpriteAssetHelper.cs
+++ Assets/Editor/SpriteAssetHelper.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-public class SpriteAssetHelper : MonoBehaviour {
+public static class SpriteAssetHelper
+{
+    [MenuItem("Asset/Create Or Update Sprite Asset")]
+    public static void Execute()
+    {
+        Object obj = Selection.activeObject;
+        CreateOrUpdateSpriteAsset(obj, null);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// 创建或者更新一个图集的资源信息
+    /// </summary>
+    /// <param name="targetObj"></param>
+    /// <param name="fullFileName"></param>
+    public static void CreateOrUpdateSpriteAsset(Object targetObj, string fullFileName)
+    {
+        if (null == targetObj || targetObj.GetType() != typeof(Texture2D))
+        {
+            return;
+        }
+
+    }
+
+    public static List<SpriteAssetInfo> GetSpriteAssetInfos(Texture2D texture2D)
+    {
+
+    }
 }
