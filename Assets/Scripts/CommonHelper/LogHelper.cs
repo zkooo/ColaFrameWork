58bde525759369716f591c12e3ade87385cbd240
马三小伙儿 <1790617178@qq.com>
2018-01-30 17:15:59 +0800

    新增WriteLog方法

--- Assets/Scripts/CommonHelper/LogHelper.cs
+++ Assets/Scripts/CommonHelper/LogHelper.cs
@@ -1,11 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 /// <summary>
 /// Unity日志助手类
 /// </summary>
-public class LogHelper : MonoBehaviour {
+public class LogHelper : MonoBehaviour
+{
+    /// <summary>
+    /// 日志文件的输出文件夹
+    /// </summary>
+    public static string outputPath;
+
+    /// <summary>
+    /// 日志文件的完整保存路径(包括文件名+后缀)
+    /// </summary>
+    public static string filePath;
+
+    /// <summary>
+    /// 日志文件的名称
+    /// </summary>
+    public static string fileName ="gamelog.txt";
+
+    private void Start()
+    {
+        outputPath = Path.Combine(Application.persistentDataPath, "logs");
+        filePath = Path.Combine(outputPath, fileName);
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
 
     /// <summary>
     /// LogCallback，Unity的Log回调
@@ -16,5 +42,18 @@ public class LogHelper : MonoBehaviour {
     public void LogCallback(string condition, string stackTrace, LogType type)
     {
         
+    }
+
+    /// <summary>
+    /// 向日志文件中写入一条消息
+    /// </summary>
+    /// <param name="message"></param>
+    private static void WriteLog(string message)
+    {
+        using (StreamWriter sw = File.AppendText(filePath))
+        {
+            string logContent = string.Format("{0:G}: {1}", System.DateTime.Now, message);
+            sw.Write(logContent);
+        }
     }
 }
