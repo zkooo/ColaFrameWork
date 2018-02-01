dce1338205f0bee682239f07adcd796dd6020eeb
马三小伙儿 <1790617178@qq.com>
2018-02-01 16:18:09 +0800

    变量命名统一为驼峰命名法，恢复被注释掉的代码

--- Assets/Scripts/UIBase/CameraAdapter.cs
+++ Assets/Scripts/UIBase/CameraAdapter.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 /// <summary>
 /// UI相机适配器,多出屏幕部分给黑边
 /// </summary>
 [RequireComponent(typeof(Camera))]
 public class CameraAdapter : MonoBehaviour
 {
+    /// <summary>
+    /// UI相机视口比例
+    /// </summary>
     public float Aspect
     {
         get
@@ -25,15 +27,18 @@ public class CameraAdapter : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// UI相机视口比例
+    /// </summary>
     private float aspect = 16 / (float)9;
 
     private bool defaultExcute = false;
 
-    private int m_defaultMask = 0;
-    private Color m_defaultColor;
+    private int defaultMask = 0;
+    private Color defaultColor;
     public bool NeedUpdateRect = false;
-    public Camera m_camera;
-    public bool bIsMainCamera = false;
+    public Camera uiCamera;
+    public bool IsMainCamera = false;
     [Header("勾选此选项宽度不加黑边")]
     public bool WidthAdapter = true;
 
@@ -49,95 +54,95 @@ public class CameraAdapter : MonoBehaviour
 
     private void OnPostRender()
     {
-        //if (NeedUpdateRect && !bIsMainCamera)
-        //{
-        //    UpdateRect();
-        //    NeedUpdateRect = false;
-        //}
+        if (NeedUpdateRect && !IsMainCamera)
+        {
+            UpdateRect();
+            NeedUpdateRect = false;
+        }
     }
 
     void OnApplicationFocus(bool hasFocus)
     {
-        //if (hasFocus)
-        //{
-        //    RefreshCameraRect();
-        //}
+        if (hasFocus)
+        {
+            RefreshCameraRect();
+        }
     }
 
     private void SetRefreshCameraRect()
     {
-        //if (!defaultExcute)
-        //{
-        //    RefreshCameraRect();
-        //    defaultExcute = true;
-        //}
+        if (!defaultExcute)
+        {
+            RefreshCameraRect();
+            defaultExcute = true;
+        }
     }
 
     public void RefreshCameraRect()
     {
-        //if (m_camera == null)
-        //{
-        //    m_camera = GetComponent<Camera>();
-        //}
-        //float aspectNow = Screen.width / (float)Screen.height;
-        //if (aspect > 0f && Mathf.Abs(aspectNow - aspect)>0.01)
-        //{
-        //    m_defaultMask = m_camera.cullingMask;
-        //    m_camera.cullingMask = LayerMask.GetMask("Nothing");
-        //    if (!defaultExcute)
-        //    {
-        //        m_defaultColor = m_camera.backgroundColor;
-        //    }
-        //    m_camera.backgroundColor = new Color(0, 0, 0, 1);
-        //    if (gameObject.activeInHierarchy)
-        //    {
-        //        NeedUpdateRect = true;
-        //        m_camera.rect = new Rect(0, 0, 1, 1);
-        //        m_camera.Render();
-        //        //m_camera.RenderDontRestore();
-        //    }
+        if (uiCamera == null)
+        {
+            uiCamera = GetComponent<Camera>();
+        }
+        float aspectNow = Screen.width / (float)Screen.height;
+        if (aspect > 0f && Mathf.Abs(aspectNow - aspect) > 0.01)
+        {
+            defaultMask = uiCamera.cullingMask;
+            uiCamera.cullingMask = LayerMask.GetMask("Nothing");
+            if (!defaultExcute)
+            {
+                defaultColor = uiCamera.backgroundColor;
+            }
+            uiCamera.backgroundColor = new Color(0, 0, 0, 1);
+            if (gameObject.activeInHierarchy)
+            {
+                NeedUpdateRect = true;
+                uiCamera.rect = new Rect(0, 0, 1, 1);
+                uiCamera.Render();
+                //uiCamera.RenderDontRestore();
+            }
 
-        //}
-        //else
-        //{
-        //    m_camera.rect = new Rect(0, 0, 1, 1);
-        //}
+        }
+        else
+        {
+            uiCamera.rect = new Rect(0, 0, 1, 1);
+        }
     }
 
 
     public void UpdateRect()
     {
-        //int defaultScreenWith = Screen.width;
-        //int defaultScreenHeight = Screen.height;
-        //float aspectNow = defaultScreenWith / (float)defaultScreenHeight;
-        //float targetH = 1f;
-        //float targetV = 1f;
-        //if (aspectNow > aspect)
-        //{
-        //    if (!WidthAdapter)
-        //    {
-        //        targetV = (defaultScreenHeight * aspect) / defaultScreenWith;
-        //    }
+        int defaultScreenWith = Screen.width;
+        int defaultScreenHeight = Screen.height;
+        float aspectNow = defaultScreenWith / (float)defaultScreenHeight;
+        float targetH = 1f;
+        float targetV = 1f;
+        if (aspectNow > aspect)
+        {
+            if (!WidthAdapter)
+            {
+                targetV = (defaultScreenHeight * aspect) / defaultScreenWith;
+            }
 
-        //}
-        //else
-        //{
-        //    targetH = defaultScreenWith / (defaultScreenHeight * aspect);
-        //}
+        }
+        else
+        {
+            targetH = defaultScreenWith / (defaultScreenHeight * aspect);
+        }
 
-        //m_camera.backgroundColor = m_defaultColor;
-        //m_camera.cullingMask = m_defaultMask;
-        //if (targetH < 1f || targetV < 1f)//上下左右都切黑边
-        ////if (targetH < 1f)//只有上下切黑边，去掉左右切黑边
-        //{
-        //    Rect rect = new Rect((1f - targetV) / 2f, (1f - targetH) / 2f, targetV, targetH);
-        //    m_camera.rect = rect;
-        //}
-        //else
-        //{
-        //    m_camera.pixelRect = new Rect(m_camera.pixelRect.x,
-        //    m_camera.pixelRect.y, Screen.width, Screen.height);
-        //    m_camera.rect = new Rect(0, 0, 1, 1);
-        //}
+        uiCamera.backgroundColor = defaultColor;
+        uiCamera.cullingMask = defaultMask;
+        if (targetH < 1f || targetV < 1f)//上下左右都切黑边
+        //if (targetH < 1f)//只有上下切黑边，去掉左右切黑边
+        {
+            Rect rect = new Rect((1f - targetV) / 2f, (1f - targetH) / 2f, targetV, targetH);
+            uiCamera.rect = rect;
+        }
+        else
+        {
+            uiCamera.pixelRect = new Rect(uiCamera.pixelRect.x,
+            uiCamera.pixelRect.y, Screen.width, Screen.height);
+            uiCamera.rect = new Rect(0, 0, 1, 1);
+        }
     }
 }
