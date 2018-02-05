86c61165272492474dc2d29f20a6b2044f826a6b
马三小伙儿 <1790617178@qq.com>
2018-02-05 19:01:32 +0800

    提交EmptyRaycast

---
+++ Assets/Scripts/UIBase/EmptyRaycast.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.UI
+{
+    public class EmptyRaycast : MaskableGraphic
+    {
+        public void EnableRayCast(bool enable) {
+            this.raycastTarget = enable;
+        }
+        protected EmptyRaycast()
+        {
+            useLegacyMeshGeneration = false;
+        }
+
+        protected override void OnPopulateMesh(VertexHelper toFill)
+        {
+            toFill.Clear();
+        }
+    }
+}
