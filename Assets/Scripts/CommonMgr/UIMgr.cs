53ab465b6bfcff9a57b3ba899440ed6c94fdd648
马三小伙儿 <1790617178@qq.com>
2018-02-06 14:23:23 +0800

    增加 点击其他地方关闭面板统一管理

--- Assets/Scripts/CommonMgr/UIMgr.cs
+++ Assets/Scripts/CommonMgr/UIMgr.cs
@@ -8,17 +8,29 @@ public class UIMgr
     private static UIMgr instance;
 
     /// <summary>
-    /// 存储UI控制脚本的字典
+    /// 用于存储所有的UI的字典
     /// </summary>
     private Dictionary<Type, UIBase> uiList;
+
+    /// <summary>
+    /// 用于存储参与点击其他地方关闭面板管理的UI的列表
+    /// </summary>
+    private List<UIBase> outTouchList;
+
+    /// <summary>
+    /// 存储要进行统一关闭面板的列表
+    /// </summary>
+    private List<UIBase> removeList;
 
     public UIMgr()
     {
         uiList = new Dictionary<Type, UIBase>();
+        outTouchList = new List<UIBase>();
+        removeList = new List<UIBase>();
 
         /*---------------UI界面控制脚本添加-------------------*/
-        UIBase ui = new UILogin(100,UIType.Common);
-        uiList.Add(typeof(UILogin),ui);
+        UIBase ui = new UILogin(100, UIType.Common);
+        uiList.Add(typeof(UILogin), ui);
     }
 
     /// <summary>
@@ -37,5 +49,38 @@ public class UIMgr
             }
         }
         return false;
+    }
+
+    /// <summary>
+    /// 设置一个UI界面参与点击其他地方关闭面板管理
+    /// </summary>
+    /// <param name="ui"></param>
+    public void SetOutTouchDisappear(UIBase ui)
+    {
+        outTouchList.Add(ui);
+    }
+
+    /// <summary>
+    /// 分发处理点击其他地方关闭面板的
+    /// </summary>
+    /// <param name="uiName"></param>
+    public void NotifyDisappear(string panelName)
+    {
+        removeList.Clear();
+        for (int i = 0; i < outTouchList.Count; i++)
+        {
+            if (null != outTouchList[i] && outTouchList[i].Name != panelName)
+            {
+                outTouchList[i].Close();
+                removeList.Add(outTouchList[i]);
+                break; //每次只关闭一个界面
+            }
+        }
+
+        //从outTouch列表中移除已经关闭的UI界面
+        for (int i = 0; i < removeList.Count; i++)
+        {
+            outTouchList.Remove(removeList[i]);
+        }
     }
 }
