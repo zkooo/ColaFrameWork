e0675af0787d676120de11f6b443de2cc3efb52f
马三小伙儿 <1790617178@qq.com>
2018-02-08 18:56:26 +0800

    ImageEffectBlur初版提交

--- Assets/Scripts/UIBase/ImageEffectUIBlur.cs
+++ Assets/Scripts/UIBase/ImageEffectUIBlur.cs
@@ -2,15 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImageEffectUIBlur : MonoBehaviour {
+public class ImageEffectUIBlur : MonoBehaviour
+{
+    /// <summary>
+    /// 渲染效果所用到的Shader
+    /// </summary>
+    private Shader effectShader;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool EnableUIBlur = false;
+
+    private bool isOpen = false;
+
+    /// <summary>
+    /// 最终渲染出来的临时Texture效果图
+    /// </summary>
+    private RenderTexture tempRtLowB = null;
+
+    /// <summary>
+    /// 主相机
+    /// </summary>
+    private Camera mainCamera;
+
+    //最终渲染出来的临时Texture效果图
+    public RenderTexture TempRtLowB
+    {
+        get
+        {
+            bool state = false;
+            if (false == EnableUIBlur)
+            {
+
+            }
+            EnableUIBlur = true;
+            isOpen = true;
+            state = true;
+            return tempRtLowB;
+        }
+        set
+        {
+            if (null == value)
+            {
+                mainCamera.enabled = true;
+                EnableUIBlur = false;
+                isOpen = false;
+                //RenderTexture.ReleaseTemporary(tempRtLowB);
+                //tempRtLowB = null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 渲染效果所用到的Shader
+    /// </summary>
+    public Shader EffecShader
+    {
+        get
+        {
+            if (null == effectShader)
+            {
+                effectShader = Shader.Find("ColaFrameWork/MobileUIBlur");
+            }
+            return effectShader;
+        }
+    }
+
+    void Awake()
+    {
+        mainCamera = GUIHelper.GetMainCamera();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
