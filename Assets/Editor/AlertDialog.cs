000b20b08caaa1ef12637dedd9a4931f12ad3ea7
马三小伙儿 <1790617178@qq.com>
2018-02-26 21:01:29 +0800

    新增AlertLevel和AlertText等级颜色

--- Assets/Editor/AlertDialog.cs
+++ Assets/Editor/AlertDialog.cs
@@ -13,21 +13,28 @@ namespace ColaFrame
         private static AlertDialog window;
         private string alertText;
         private Color defaultColor;
+        private Dictionary<AlertLevel, Color> textColorDic = new Dictionary<AlertLevel, Color>()
+        {
+            {AlertLevel.Info, Color.white },
+            {AlertLevel.Debug, Color.green },
+            {AlertLevel.Warn, Color.yellow },
+            {AlertLevel.Error, Color.red },
+        };
 
-        public static void PopUp(string alertText)
+        public static void PopUp(string alertText, AlertLevel alertLevel = AlertLevel.Info)
         {
             window = EditorWindow.GetWindow(typeof(AlertDialog), true, "提示框") as AlertDialog;
             window.position = new Rect(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(500, 100));
-            window.SetText(alertText);
+            window.SetText(alertText,alertLevel);
             window.Show();
             window.Focus();
         }
 
-        private void SetText(string alertText)
+        private void SetText(string alertText,AlertLevel alertLevel)
         {
             this.alertText = alertText;
             defaultColor = GUI.color;
-            GUI.color = Color.red;
+            GUI.color = textColorDic[alertLevel];
         }
 
         private void OnGUI()
@@ -50,6 +57,29 @@ namespace ColaFrame
             GUI.color = defaultColor;
         }
 
+    }
+
+    /// <summary>
+    /// 提示的级别
+    /// </summary>
+    public enum AlertLevel : byte
+    {
+        /// <summary>
+        /// 普通信息，白色
+        /// </summary>
+        Info = 1,
+        /// <summary>
+        /// 调试信息，绿色
+        /// </summary>
+        Debug = 2,
+        /// <summary>
+        /// 警告信息，黄色
+        /// </summary>
+        Warn = 3,
+        /// <summary>
+        /// 错误信息，红色
+        /// </summary>
+        Error = 4,
     }
 
 }
