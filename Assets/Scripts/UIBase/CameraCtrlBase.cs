f7c643676ab6bdc7004e63deba4e8243972793f2
马三小伙儿 <1790617178@qq.com>
2018-02-27 20:59:18 +0800

    去掉CameraController接口 完善CameraControlBase

--- Assets/Scripts/UIBase/CameraCtrlBase.cs
+++ Assets/Scripts/UIBase/CameraCtrlBase.cs
@@ -1,14 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 /// <summary>
-/// 相机控制脚本的接口
+/// 相机控制脚本的基类
 /// </summary>
-public interface ICameraController
+public class CameraCtrlBase
 {
-    
-}
+    private Transform transform;
+    private float fov = 45;
 
-public class CameraCtrlBase : MonoBehaviour {
+    public CameraCtrlBase()
+    {
+        GameObject go = new GameObject();
+        go.name = this.GetType().ToString();
+        this.transform = go.transform;
+    }
+
+    public Transform Transform
+    {
+        get { return transform; }
+    }
+
+    public float Fov
+    {
+        get { return fov; }
+        set { fov = value; }
+    }
+
+    public virtual bool enable { get; set; }
+
+    /// <summary>
+    /// 强制就位
+    /// </summary>
+    public virtual void ForcePosition() { }
+
+    public virtual void OnSetTouchDeltaX(float value) { }
+    public virtual void OnSetTouchDeltaY(float value) { }
+
+    public virtual void OnLateUpdate(float deltaTime, Vector3 shakeOffset) { }
 }
