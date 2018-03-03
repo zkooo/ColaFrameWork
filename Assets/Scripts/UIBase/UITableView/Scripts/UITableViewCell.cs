6bd1cfe0b83caa7d114475833e66052f94f4981d
马三小伙儿 <1790617178@qq.com>
2018-03-03 18:38:15 +0800

    添加UITableViewCell组件

---
+++ Assets/Scripts/UIBase/UITableView/Scripts/UITableViewCell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LuaInterface;
+
+public class UITableViewCell : MonoBehaviour {
+
+    [HideInInspector]
+    public int index;
+    [HideInInspector]
+    public RectTransform cacheTransform;
+    void Awake()
+    {
+        cacheTransform = transform as RectTransform;
+        Debug.Assert(cacheTransform != null, "transform should be RectTransform");
+    }
+}
