d46ad2cae17b851f4165c02bc9b7029513c3fe16
马三小伙儿 <1790617178@qq.com>
2018-03-03 18:38:35 +0800

    添加testUITableView测试脚本

---
+++ Assets/Scripts/UIBase/UITableView/Scripts/TestTableView.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTableView : MonoBehaviour {
+
+    private UITableView tableView;
+
+    public int CellCount = 10;
+
+    public GameObject cellPrefab = null;
+	// Use this for initialization
+	void Start () {
+        InitTableView();
+	}
+
+    void InitTableView()
+    {
+        tableView = GetComponent<UITableView>();
+        tableView.SetCellByIndexCallback(GetCellByIndex);
+        tableView.Reload(100);
+    }
+
+    /// <summary>
+    /// tableview callback
+    /// </summary>
+    /// <returns></returns>
+    int TotalCellCount()
+    {
+        return CellCount;
+    }
+
+    /// <summary>
+    /// tableview callback
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    UITableViewCell GetCellByIndex(int index)
+    {
+        UITableViewCell cell = tableView.GetReUseCell();
+        if(cell == null)
+        {
+            GameObject go = Instantiate(cellPrefab) as GameObject;
+            cell = go.GetComponent<UITableViewCell>();
+        }
+        cell.index = index;
+        Text label = cell.transform.GetComponentInChildren<Text>();
+        label.text = "cell_" + index;
+        return cell;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
