0d1752274f2a164919a8e29056f0902bd96387e0
马三小伙儿 <1790617178@qq.com>
2018-03-18 21:50:16 +0800

    对TaleviewCell组件进行拓展，复写OnDestroy函数

--- Assets/Scripts/UIBase/UITableView/Scripts/UITableViewCell.cs
+++ Assets/Scripts/UIBase/UITableView/Scripts/UITableViewCell.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UITableViewCell : MonoBehaviour {
+public class UITableViewCell : MonoBehaviour
+{
 
     [HideInInspector]
     public int index;
     [HideInInspector]
     public RectTransform cacheTransform;
+    [HideInInspector]
+    public Dictionary<string, Component> extenParams;
+
     void Awake()
     {
         cacheTransform = transform as RectTransform;
+        extenParams = new Dictionary<string, Component>();
         Debug.Assert(cacheTransform != null, "transform should be RectTransform");
     }
+
+    void OnDestroy()
+    {
+        index = -1;
+        cacheTransform = null;
+        if (null != extenParams)
+        {
+            extenParams.Clear();
+        }
+        extenParams = null;
+    }
+
+
 }
