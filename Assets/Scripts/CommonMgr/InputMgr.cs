0a245164932e3884d4f1431f291efb7697371433
马三小伙儿 <1790617178@qq.com>
2018-03-31 15:22:59 +0800

    新增输入输出控制器

---
+++ Assets/Scripts/CommonMgr/InputMgr.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ColaFramework框架输入输出管理器
+/// </summary>
+public class InputMgr : MonoBehaviour
+{
+    private bool isQuit = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (Input.GetKey(KeyCode.Escape))
+	    {
+	        isQuit = true;
+	    }
+	}
+
+    private void OnGUI()
+    {
+        if (isQuit)
+        {
+            GUI.Box(new Rect(0,0,200,100),"确定要退出游戏？");
+
+            if (GUI.Button(new Rect(20, 50, 70, 30), "确定"))
+            {
+                Application.Quit();
+            }
+
+            if (GUI.Button(new Rect(110, 50, 70, 30), "取消"))
+            {
+                isQuit = false;
+            }
+        }
+    }
+
+
+}
+
