54b6ad1626b7167bef6a3845a1f24fb293f13f47
马三小伙儿 <1790617178@qq.com>
2018-04-10 12:25:27 +0800

    DropdownEditor提交

---
+++ Assets/Editor/DropdownExtEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine.UI.Extensions;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(DropdownExtension))]
+public class DropdownExtEditor : DropdownEditor
+{
+    SerializedProperty openMark;
+    SerializedProperty closeMark;
+    SerializedProperty useContentSize;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        // Setup the SerializedProperties.
+        openMark = serializedObject.FindProperty("openMark");
+        closeMark = serializedObject.FindProperty("closeMark");
+        useContentSize = serializedObject.FindProperty("useContentSize");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(openMark, new GUIContent("openMark"));
+        EditorGUILayout.PropertyField(closeMark, new GUIContent("closeMark"));
+        EditorGUILayout.PropertyField(useContentSize, new GUIContent("useContentSize"));
+
+        // Apply changes to the serializedProperty - always do this in the end of OnInspectorGUI.
+        serializedObject.ApplyModifiedProperties();
+    }
+}
