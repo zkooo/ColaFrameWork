906463ad942a1cdbb62790d1fa924a53be671855
马三小伙儿 <1790617178@qq.com>
2018-05-13 19:15:02 +0800

    新增测试脚本

---
+++ Assets/Scripts/_TestScripts/UIHintTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHintTest : UIBase
+{
+    public UIHintTest(int resId, UILevel uiLevel) : base(resId, uiLevel)
+    {
+    }
+
+    public UIHintTest(GameObject panel, GameObject parent, UILevel uiLevel) : base(panel, parent, uiLevel)
+    {
+    }
+
+    protected override void onClick(GameObject obj)
+    {
+        if (obj.name == "bg")
+        {
+            Debug.LogWarning("点击了bg按钮");
+        }
+    }
+}
