4bbc39492c79e63758adcd118983c566eb49305f
马三小伙儿 <1790617178@qq.com>
2018-05-19 17:50:33 +0800

    添加SorterTag组件，用于界面层级排序

---
+++ Assets/Scripts/UIBase/SorterTag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SorterTag : MonoBehaviour
+{
+    public int Space3D = 0;
+
+    public int GetSorter()
+    {
+        return transform.GetSiblingIndex();
+    }
+
+    public void SetSorter(int index)
+    {
+        if (index == -1)
+        {
+            transform.SetAsFirstSibling();
+        }
+        else
+        {
+            transform.SetSiblingIndex(index);
+        }
+    }
+
+    public void SetSpace3D(int z)
+    {
+        //Debug.LogError(z);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y,z);
+    }
+}
