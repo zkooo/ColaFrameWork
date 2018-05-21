3339a9d0e0e97ec898e4d56cca32bbcdf4bc0ff2
马三小伙儿 <1790617178@qq.com>
2018-05-21 16:29:44 +0800

    完善CopyAsset对话框，并添加测试场景

--- Assets/Scripts/CommonHelper/UICopyingAssetHelper.cs
+++ Assets/Scripts/CommonHelper/UICopyingAssetHelper.cs
@@ -10,22 +10,54 @@ public class UICopyingAssetHelper : UIBase
     private static UICopyingAssetHelper instance = null;
     private static RawImage background;
     private static Text progressText;
+    private bool isCreate = false;
 
-    private UICopyingAssetHelper():base()
+    private UICopyingAssetHelper() : base()
     {
-        var uiGameobject =   Resources.Load<GameObject>(resPath);
-        CreateWithGO(uiGameobject,UILevel.Top);
+        GameObject prefab = Resources.Load<GameObject>(resPath);
+        if (null == prefab)
+        {
+            InitError();
+        }
+        GameObject uiGameobject = GameObject.Instantiate(prefab);
+        if (null == uiGameobject)
+        {
+            InitError();
+        }
+
+        uiGameobject.name = prefab.name;
+        uiGameobject.transform.SetParent(GUIHelper.GetUIRootObj().transform, false);
+        uiGameobject.transform.localScale = prefab.transform.localScale;
+        uiGameobject.transform.localPosition = prefab.transform.localPosition;
+        uiGameobject.transform.localRotation = prefab.transform.localRotation;
+        CreateWithGO(uiGameobject, UILevel.Top);
     }
 
+    private void InitError()
+    {
+        Debug.LogError("拷贝解压资源预制加载失败！");
+        Application.Quit();
+    }
 
-    public UICopyingAssetHelper Instance()
+    /// <summary>
+    /// 本UI设计单例，便于访问操作等
+    /// </summary>
+    /// <returns></returns>
+    public static UICopyingAssetHelper Instance()
     {
         if (null == instance)
         {
             instance = new UICopyingAssetHelper();
         }
-
         return instance;
+    }
+
+    public override void Open()
+    {
+        //如果已经创建过一次了就不再二次创建
+        if (isCreate) return;
+        base.Open();
+        isCreate = true;
     }
 
     public override void OnCreate()
@@ -35,8 +67,13 @@ public class UICopyingAssetHelper : UIBase
         progressText = Panel.GetComponentByPath<Text>("progress_text");
     }
 
-    public void UpdateUI(int progress,int totalCount,string HintText)
+    public void UpdateUI(int progress, int totalCount, string HintText)
     {
+        //首次调用刷新的时候创建面板
+        if (!isCreate)
+        {
+            Open();
+        }
         progressText.text = string.Format("{0}:({1}/{2})", HintText, progress, totalCount);
     }
 
@@ -45,5 +82,14 @@ public class UICopyingAssetHelper : UIBase
     {
         uiCreateType = UICreateType.Res;
         base.Destroy();
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        instance = null;
+        background = null;
+        progressText = null;
+        isCreate = false;
     }
 }
---
+++ Assets/Scripts/_TestScripts/UICopyAssetTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICopyAssetTest : MonoBehaviour
+{
+
+    private int index = 1;
+    private int totalCount = 12;
+    private int timerID = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        timerID = TimeHelper.SetRepeatTimer(() =>
+        {
+            if (index <= totalCount)
+            {
+                UICopyingAssetHelper.Instance().UpdateUI(index, totalCount, "正在拷贝资源...");
+                index++;
+            }
+            else
+            {
+                UICopyingAssetHelper.Instance().Close();
+                TimeHelper.KillTimer(timerID);
+            }
+        }, 1.0f);
+    }
+
+}
