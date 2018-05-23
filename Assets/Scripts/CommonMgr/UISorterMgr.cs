549c275be428a3306be04ec05ef5ac2262ab2353
马三小伙儿 <1790617178@qq.com>
2018-05-23 15:30:54 +0800

    实现完善SortIndexSetter方法

--- Assets/Scripts/CommonMgr/UISorterMgr.cs
+++ Assets/Scripts/CommonMgr/UISorterMgr.cs
@@ -10,12 +10,14 @@ public class UISorterMgr : ISorter
     private int minSortIndex = 0;
     private int maxSortIndex = 0;
     private List<UIBase> uiSortList;
+    private List<Canvas> canvasSortList;
 
     public UISorterMgr(int minIndex, int maxIndex)
     {
         minSortIndex = minIndex;
         maxSortIndex = maxIndex;
         uiSortList = new List<UIBase>();
+        canvasSortList = new List<Canvas>();
     }
 
     /// <summary>
@@ -26,7 +28,26 @@ public class UISorterMgr : ISorter
     /// <returns></returns>
     public int SortIndexSetter(GameObject panel, int sortIndex)
     {
-        throw new System.NotImplementedException();
+        if (null == panel)
+        {
+            Debug.LogWarning("参与排序的ui不能为空！");
+            return 0;
+        }
+        canvasSortList.Clear();
+        var canvasList = panel.GetComponentsInChildren<Canvas>(true);
+        for (int i = 0; i < canvasList.Length; i++)
+        {
+            canvasSortList.Add(canvasList[i]);
+        }
+        canvasSortList.Sort((x, y) => { return x.sortingOrder.CompareTo(y.sortingOrder); });
+
+        for (int i = 0; i < canvasSortList.Count; i++)
+        {
+            canvasSortList[i].sortingOrder = sortIndex;
+            //DropDown组件关闭按钮的层级为DropDown层级减一，所以多加一个间隔
+            sortIndex += 2;
+        }
+        return sortIndex + 2;
     }
 
     public int SortTagIndexSetter(GameObject panel, int sortIndex)
