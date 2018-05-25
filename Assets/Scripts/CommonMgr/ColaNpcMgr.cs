7a5ed69a7c20bed718c926e9adb591bbd41b8b43
马三小伙儿 <1790617178@qq.com>
2018-05-25 18:02:35 +0800

    新增ColaFramework的NPC管理器

---
+++ Assets/Scripts/CommonMgr/ColaNpcMgr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// ColaFramework的NPC管理器
+/// </summary>
+public class ColaNpcMgr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
