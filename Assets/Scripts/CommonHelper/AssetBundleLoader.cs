caaefa7e537957f979cc4c8afd5af2d183cb65c4
马三小伙儿 <1790617178@qq.com>
2018-07-03 18:10:20 +0800

    添加AssetBundleLoader和Mgr

---
+++ Assets/Scripts/CommonHelper/AssetBundleLoader.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    /// <summary>
+    /// 异步通过路径加载Bundle
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="callback"></param>
+    public void LoadAssetBundleAsync(string path, Action<AssetBundle> callback, BundleLoadType type = BundleLoadType.File, int version = 0)
+    {
+        switch (type)
+        {
+            case BundleLoadType.File:
+                StartCoroutine(LoadAsyncCoroutine(path, callback));
+                break;
+            case BundleLoadType.WWW:
+                StartCoroutine(LoadFromWWWCoroutine(path, callback));
+                break;
+            case BundleLoadType.LoadFromCacheOrDownload:
+                StartCoroutine(LoadFromWWWCacheOrDownload(path, callback, version));
+                break;
+            case BundleLoadType.WebRequest:
+                StartCoroutine(LoadFromWebRequest(path, callback, version));
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 从文件加载bundle的携程
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    private IEnumerator LoadAsyncCoroutine(string path, Action<AssetBundle> callback)
+    {
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(path);
+        yield return request;
+        callback(request.assetBundle);
+    }
+
+    /// <summary>
+    /// 使用WWW加载bundle的携程
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    private static IEnumerator LoadFromWWWCoroutine(string path, Action<AssetBundle> callback)
+    {
+        WWW www = new WWW(path);
+        yield return www;
+        callback(www.assetBundle);
+        www.Dispose();
+        www = null;
+    }
+
+    /// <summary>
+    ///  使用WWWCacheOrDownload加载bundle的携程
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="callback"></param>
+    /// <param name="version"></param>
+    /// <returns></returns>
+    public static IEnumerator LoadFromWWWCacheOrDownload(string path, Action<AssetBundle> callback, int version)
+    {
+        while (!Caching.ready)
+        {
+            yield return null;
+        }
+        WWW www = WWW.LoadFromCacheOrDownload(path, version);
+        yield return www;
+        callback(www.assetBundle);
+        www.Dispose();
+        www = null;
+    }
+
+    public static IEnumerator LoadFromWebRequest(string path, Action<AssetBundle> callback, int version)
+    {
+        UnityWebRequest request = UnityWebRequest.GetAssetBundle(path, (uint)version, 0);
+        yield return request.SendWebRequest();
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+        callback(bundle);
+        request.Dispose();
+        request = null;
+    }
+}
+
+/// <summary>
+/// BundleLoader的方式
+/// </summary>
+public enum BundleLoadType : byte
+{
+    File,
+    WWW,
+    LoadFromCacheOrDownload,
+    WebRequest,
+}
---
+++ Assets/Scripts/CommonMgr/AssetBundleMgr.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// AssetBundle管理器
+/// </summary>
+public class AssetBundleMgr
+{
+    public static readonly string streamingAssetPath = Application.streamingAssetsPath;
+    public static readonly string persistentDataPath = Application.persistentDataPath;
+    public static readonly string wwwStreamingAssetPath =
+#if UNITY_ANDROID
+        Application.streamingAssetsPath;
+#else
+        "file://" + Application.streamingAssetsPath;
+#endif
+    private static AssetBundleMgr instance;
+    private static AssetBundleLoader assetBundleLoader;
+    private AssetBundleManifest mainManifest;
+    private List<AssetBundle> depsBundles;
+
+    private AssetBundleMgr()
+    {
+        if (null == assetBundleLoader)
+        {
+            GameObject go = new GameObject("AssetBundleLoader");
+            GameObject.DontDestroyOnLoad(go);
+            assetBundleLoader = go.AddComponent<AssetBundleLoader>();
+            depsBundles = new List<AssetBundle>();
+            LoadMainManifest();
+        }
+    }
+
+    public static AssetBundleMgr GetInstance()
+    {
+        if (null == instance)
+        {
+            instance = new AssetBundleMgr();
+        }
+        return instance;
+    }
+
+    /// <summary>
+    /// 通过资源名获取所在bundle的名称
+    /// </summary>
+    /// <param name="assetName"></param>
+    /// <returns></returns>
+    string GetBundleName(string assetName)
+    {
+        if (assetName == "AssetBundleManifest")
+        {
+            return "StreamingAssets";
+        }
+        return assetName.ToLower();
+    }
+
+    #region AssetBundle加载
+
+    /// <summary>
+    /// 从StreamingAssetPath加载(同步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <returns></returns>
+    public static AssetBundle LoadFromStreamingAssetPath(string bundleName)
+    {
+        return AssetBundle.LoadFromFile(Path.Combine(streamingAssetPath, bundleName));
+    }
+
+    /// <summary>
+    /// 从PersistantDataPath加载(同步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <returns></returns>
+    public static AssetBundle LoadFromPersistantDataPath(string bundleName)
+    {
+        return AssetBundle.LoadFromFile(Path.Combine(persistentDataPath, bundleName));
+    }
+
+    /// <summary>
+    /// 从StreamingAssetPath加载(异步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <returns></returns>
+    public static void LoadFromStreamingAssetPathAsync(string bundleName, Action<AssetBundle> callback)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(Path.Combine(streamingAssetPath, bundleName), callback);
+    }
+
+    /// <summary>
+    /// 从PersistantDataPath加载(异步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <param name="callback"></param>
+    public static void LoadFromPersistantDataPathAsync(string bundleName, Action<AssetBundle> callback)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(Path.Combine(persistentDataPath, bundleName), callback);
+    }
+
+    /// <summary>
+    /// 使用WWW从本地加载bundle(异步)
+    /// </summary>
+    /// <param name="bundleName"></param>
+    /// <param name="callback"></param>
+    /// <param name="version"></param>
+    public static void LoadFromWWWLocalAsync(string bundleName, Action<AssetBundle> callback, int version)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(Path.Combine(wwwStreamingAssetPath, bundleName), callback, BundleLoadType.WWW, version);
+    }
+
+    /// <summary>
+    /// 使用WWW从网络URL地址加载bundle(异步)
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="callback"></param>
+    /// <param name="version"></param>
+    public static void LoadFromWWWUrlAsync(string url, Action<AssetBundle> callback, int version)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(url, callback, BundleLoadType.WWW, version);
+    }
+
+    /// <summary>
+    /// 使用WWWCacheOrDownload从网络URL加载bundle(异步)
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="callback"></param>
+    /// <param name="version"></param>
+    public static void LoadFromWWWCacheOrDownloadAsync(string url, Action<AssetBundle> callback, int version)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(url, callback, BundleLoadType.LoadFromCacheOrDownload, version);
+    }
+
+    public static void LoadFromWebRequestAsync(string url, Action<AssetBundle> callback, int version)
+    {
+        assetBundleLoader.LoadAssetBundleAsync(url, callback, BundleLoadType.WebRequest, version);
+    }
+
+    #endregion
+
+    #region 资源加载
+
+    /// <summary>
+    /// 从StreamingAsset中加载资源(同步)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="assetName"></param>
+    /// <returns></returns>
+    public T LoadAssetFromStreamingAsset<T>(string assetName) where T : UnityEngine.Object
+    {
+        GetDependenciesByName(assetName);
+        AssetBundle assetBundle = LoadFromStreamingAssetPath(GetBundleName(assetName));
+        T obj = assetBundle.LoadAsset<T>(assetName);
+        assetBundle.Unload(false);
+        //ReleaseAllDependencies();
+        return obj;
+    }
+
+    /// <summary>
+    /// 从PersistantData中加载资源(同步)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="assetName"></param>
+    /// <returns></returns>
+    public T LoadAssetFromPersistant<T>(string assetName) where T : UnityEngine.Object
+    {
+        GetDependenciesByName(assetName);
+        AssetBundle assetBundle = LoadFromPersistantDataPath(GetBundleName(assetName));
+        T obj = assetBundle.LoadAsset<T>(assetName);
+        assetBundle.Unload(false);
+        //ReleaseAllDependencies();
+        return obj;
+    }
+
+    public void LoadMainManifest()
+    {
+        mainManifest = LoadAssetFromStreamingAsset<AssetBundleManifest>("AssetBundleManifest");
+    }
+
+    private void GetDependenciesByName(string assetName)
+    {
+        if ("AssetBundleManifest" == assetName) return;
+        string[] deps = mainManifest.GetAllDependencies(GetBundleName(assetName));
+        depsBundles.Clear();
+        for (int i = 0; i < deps.Length; i++)
+        {
+            AssetBundle assetBundle = LoadFromStreamingAssetPath(GetBundleName(deps[i]));
+            depsBundles.Add(assetBundle);
+        }
+    }
+
+    public void ReleaseAllDependencies()
+    {
+        for (int i = 0; i < depsBundles.Count; i++)
+        {
+            depsBundles[i].Unload(false);
+        }
+    }
+
+    #endregion
+}
