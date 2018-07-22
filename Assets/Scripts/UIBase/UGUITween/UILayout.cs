3146fd37011b1571d007974c43051df3a5daaa79
马三小伙儿 <1790617178@qq.com>
2018-07-22 22:43:03 +0800

    修改UILayout的注释

--- Assets/Scripts/UIBase/UGUITween/UILayout.cs
+++ Assets/Scripts/UIBase/UGUITween/UILayout.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿//----------------------------------------------
+//            ColaFramework
+// Copyright © 2018-2020 ColaFramework 马三小伙儿
+//----------------------------------------------
+
+using UnityEngine;
 using System.Collections;
 
 /// <summary>
