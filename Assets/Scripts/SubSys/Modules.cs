30d15ed38debfccdd7a18e0b9b4338b5da2a7219
马三小伙儿 <1790617178@qq.com>
2018-08-07 21:48:45 +0800

    补全注释

--- Assets/Scripts/SubSys/Modules.cs
+++ Assets/Scripts/SubSys/Modules.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 /// <summary>
 /// 存储所有的系统
+/// 将新增的系统添加到AllModules列表中即可，ModuleMgr会自动记录\注册所有的系统
 /// </summary>
 public class Modules
 {
@@ -18,6 +19,8 @@ public class Modules
     private void Init()
     {
         AllModules = new List<ModuleBase>();
+
+        //仿照这个添加新的系统到列表中
         AllModules.Add(new LoginModule());
     }
 }
