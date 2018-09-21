40e4da4d9b20bebe92fc4f457d6271e8afece129
马三小伙儿 <1790617178@qq.com>
2018-09-21 19:57:38 +0800

    UI组件序列化存储脚本

---
+++ Assets/Scripts/UIBase/UGUIExtensions/UIComponentCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.UI.Extensions
+{
+    /// <summary>
+    /// ColaFramework框架，UI组件序列化存储脚本
+    /// </summary>
+    sealed public class UIComponentCollection : MonoBehaviour
+    {
+        [SerializeField]
+        internal List<Component> components = new List<Component>();
+
+        public T Get<T>(int index) where T : Component
+        {
+            return (T)components[index];
+        }
+    }
+}
+
