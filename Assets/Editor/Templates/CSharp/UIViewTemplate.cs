f88522cefba3ab1e3352136372b8bdac273295b2
马三小伙儿 <1790617178@qq.com>
2018-09-25 21:01:46 +0800

    添加对应的脚本模板文件

---
+++ Assets/Editor/Templates/CSharp/UIViewTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// xxxUI界面
+/// </summary>
+public class UIViewTemplate : UIBase {
+
+    /// <summary>
+    /// 打开一个UI界面
+    /// </summary>
+    public override void Open()
+    {
+        base.Open();
+    }
+}
