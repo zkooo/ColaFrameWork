9d94acf1c7cde70791750c7dfb54097e1e3184c6
马三小伙儿 <1790617178@qq.com>
2018-09-30 16:09:03 +0800

    新增CreateCSharpUIView方法，添加对应的EndAction

---
+++ Assets/Editor/CreateScriptEditor/CreateScriptsEditor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Text;
+using UnityEditor.ProjectWindowCallback;
+
+#if UNITY_EDITOR
+/// <summary>
+/// 用于创建代码模版的编辑器类(UI\Model等类)
+/// </summary>
+public static class CreateScriptsEditor
+{
+    #region 共有变量
+    private static readonly string CSharpTemplateUIViewPath = "Assets/Editor/CreateScriptEditor/Templates/CSharp/UIViewTemplate.txt";
+    #endregion
+
+    #region 共用方法
+    /// <summary>
+    /// 获取在编辑器中选择的路径
+    /// </summary>
+    /// <returns></returns>
+    static string GetSelectedPath()
+    {
+        string path = "Assets";
+        foreach (UnityEngine.Object obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+        {
+            path = AssetDatabase.GetAssetPath(obj);
+            if (!string.IsNullOrEmpty(path) && File.Exists(path))
+            {
+                path = Path.GetDirectoryName(path);
+                break;
+            }
+        }
+        return path;
+    }
+    #endregion
+
+    #region 创建C#模版
+    [MenuItem("Assets/Create/C#/UIView", false, 70)]
+    public static void CreateCSharpUIView()
+    {
+        string basePath = GetSelectedPath();
+        string templateFullPath = CSharpTemplateUIViewPath;
+
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
+            ScriptableObject.CreateInstance<CreateCSharpScriptEndAction>(),
+            basePath + "/NewUIView.cs",
+            null,
+            templateFullPath);
+    }
+    #endregion
+
+    #region 创建Lua模版
+    #endregion
+}
+
+#endif
