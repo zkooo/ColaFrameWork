1b53059d4713f320d1a71837b0aa56ee2758a9dd
马三小伙儿 <1790617178@qq.com>
2018-10-10 20:44:48 +0800

    新增ColaFramework框架UI预制件导出工具

---
+++ Assets/Editor/ColaUIEditor/ColaUIViewExporter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ColaFramework框架UI预制件导出工具
+/// </summary>
+public class ColaUIViewExporter 
+{
+}
