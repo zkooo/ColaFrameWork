1d31b681007820b32e777a53c90ec271b1b611b5
马三小伙儿 <1790617178@qq.com>
2018-10-21 19:11:39 +0800

    对ToLua进行定义与一些配置

--- Assets/Editor/Custom/CustomSettings.cs
+++ Assets/Editor/Custom/CustomSettings.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 
 public static class CustomSettings
 {
-    public static string saveDir = Application.dataPath + "/Source/Generate/";    
-    public static string toluaBaseType = Application.dataPath + "/ToLua/BaseType/";    
+    public static string saveDir = Application.dataPath + "/Scripts/Generate/";    
+    public static string toluaBaseType = Application.dataPath + "/3rd/ToLua/BaseType/";    
 
     //导出时强制做为静态类的类型(注意customTypeList 还要添加这个类型才能导出)
     //unity 有些类作为sealed class, 其实完全等价于静态类
