545a33074ff4ecce21f71fe9f5c446789895ccbd
马三小伙儿 <1790617178@qq.com>
2018-11-01 23:56:25 +0800

    添加middleclass测试

--- Assets/Lua/Main.lua
+++ Assets/Lua/Main.lua
@@ -20,6 +20,30 @@ function Main()
 	local UIView = require("UIView").Instance()
 	local result = UIView:TestFunc(123)
 	print("------->结果",result)
+
+    local class = require 'middleclass'
+
+    local Fruit = class('Fruit') -- 'Fruit' is the class' name
+
+    function Fruit:initialize(sweetness)
+        self.sweetness = sweetness
+    end
+
+    Fruit.sweetness_threshold = 5 -- class variable (also admits methods)
+
+    function Fruit:isSweet()
+        return self.sweetness > Fruit.sweetness_threshold
+    end
+
+    local Lemon = class('Lemon', Fruit) -- subclassing
+
+    function Lemon:initialize()
+        Fruit.initialize(self, 1) -- invoking the superclass' initializer
+    end
+
+    local lemon = Lemon:new()
+
+    print(lemon:isSweet()) -- false
 end
 
 --场景切换通知
