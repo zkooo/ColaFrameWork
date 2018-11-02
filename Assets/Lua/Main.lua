a968fa4ef89e50503639e692270b4822112a3d74
马三小伙儿 <1790617178@qq.com>
2018-11-02 20:16:35 +0800

    修改初始化流程

--- Assets/Lua/Main.lua
+++ Assets/Lua/Main.lua
@@ -1,5 +1,4 @@
 --主入口函数。从这里开始lua逻辑
-local luaLogHelper = require("LuaLogHelper")
 local rawset = rawset
 
 -- 全局函数
@@ -9,48 +8,24 @@ function define(name,value)
 end
 
 local function initialize()
-	luaLogHelper.initialize()
+	LuaLogHelper.initialize()
+end
+
+-- 在此处定义注册一些全局变量
+local function gloablDefine()
+	define("LuaLogHelper",require("LuaLogHelper"))
+end
+
+-- 初始化一些参数
+local function initParam()
+	-- 初始化随机种子
+	math.randomseed(tostring(os.time()):reverse():sub(1, 6))
 end
 
 function Main()
+	gloablDefine()
+	initParam()
 	initialize()
-	print("logic start")
-	local t = {"a",'b','c'}
-	local tt = {}
-	tt["key"] = 123
-	tt["value"] = 456
-	local ttt = {t,tt}
-	print("------>Test","abd",{5,4,3,2,1,t,tt,ttt})
-	warn("------->warnTest",debug.traceback())
-	error("---------->ErrorTest")
-
-	local UIView = require("UIView").Instance()
-	local result = UIView:TestFunc(123)
-	print("------->结果",result)
-
-    local class = require 'middleclass'
-
-    local Fruit = class('Fruit') -- 'Fruit' is the class' name
-
-    function Fruit:initialize(sweetness)
-        self.sweetness = sweetness
-    end
-
-    Fruit.sweetness_threshold = 5 -- class variable (also admits methods)
-
-    function Fruit:isSweet()
-        return self.sweetness > Fruit.sweetness_threshold
-    end
-
-    local Lemon = class('Lemon', Fruit) -- subclassing
-
-    function Lemon:initialize()
-        Fruit.initialize(self, 1) -- invoking the superclass' initializer
-    end
-
-    local lemon = Lemon:new()
-
-    print(lemon:isSweet()) -- false
 end
 
 --场景切换通知
