1833bf6948b2c5a40e59f27d59f02b8f295d10c0
马三小伙儿 <1790617178@qq.com>
2018-11-13 14:22:04 +0800

    修改UI组件收集器

---
+++ Assets/Scripts/UIBase/UGUIExtensions/UIComponents.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngine.UI.Extensions
+{
+    /// <summary>
+    /// ColaFramework框架，UI组件序列化存储脚本
+    /// </summary>
+    public class UIComponents : MonoBehaviour
+    {
+        public List<Component> components = new List<Component>();
+    }
+}
+
