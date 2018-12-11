4ade6589438a18ff5d5bc5003d950e3bcdd158c1
马三小伙儿 <1790617178@qq.com>
2018-12-11 21:04:46 +0800

    完善UIMananger

--- Assets/Lua/Mgrs/UIManager.lua
+++ Assets/Lua/Mgrs/UIManager.lua
@@ -4,4 +4,20 @@
 
 local UIManager = Class("UIManager")
 
+UIManager._instance = nil
+
+function UIManager:initialize()
+    self.uiList = {}  -- 存储打开的UI列表
+    self.outTouchList = {} -- 用于存储参与点击其他地方关闭面板管理的UI的列表
+    self.removeList = {} -- 存储要进行统一关闭面板的列表
+    self.recordList = {} -- 存储统一隐藏/恢复显示的UI列表
+end
+
+function UIManager:Instance()
+    if nil == UIManager._instance then
+        UIManager._instance = UIManager:new()
+    end
+    return UIManager._instance
+end
+
 return
--- Assets/Lua/Mgrs/UISorter.lua
+++ Assets/Lua/Mgrs/UISorter.lua
@@ -1,3 +1,7 @@
 ---
 --- UISorter UI界面层级排序
 ---
+
+local UISorter = Class("UISorter")
+
+return UISorter
