b5847267812e67dc4a64277d661f08eee2587ea2
马三小伙儿 <1790617178@qq.com>
2019-01-28 18:48:04 +0800

    logHelper改造

--- Assets/Lua/Utilitys/LuaLogHelper.lua
+++ Assets/Lua/Utilitys/LuaLogHelper.lua
@@ -6,32 +6,31 @@ local logHelper = {}
 
 local isLog = true  -- 是否打印日志
 local tablePritDepth = 5 --table最深的打印层次
-local logFunc = nil
 
 -- 普通日志
 function logHelper.debug(...)
-	if logFunc then
+	if LogFunction then
 		LogFunction(3,...)
 	end
 end
 
 -- 警告
 function logHelper.warn(...)
-	if logFunc then
+	if LogFunction then
 		LogFunction(2,...)
 	end
 end
 
 -- 错误
 function logHelper.error(...)
-	if logFunc then
+	if LogFunction then
 		LogFunction(0,...)
 	end
 end
 
 -- 初始化
 function logHelper.initialize()
-	logFunc = LogFunction
+
 end
 
 -- 函数注册到全局
