4067ac0ecb46ea83dfc6a36f4eaa8b27af155dd4
马三小伙儿 <1790617178@qq.com>
2019-02-20 20:51:34 +0800

    添加UISorter一些待实现的接口

--- Assets/Lua/Mgrs/UISorter.lua
+++ Assets/Lua/Mgrs/UISorter.lua
@@ -18,4 +18,29 @@ function uiSorter.Create(minSortIndex, maxSortIndex)
     return uiSorter
 end
 
+-- 对某个Gameobject下的Canvas进行动态排序功能,识别子canvas，根据sortingOrder对canvas的排序
+function uiSorter:SortIndexSetter(panel,sortIndex)
+
+end
+
+-- 设置UI的SortTag,根据显示修改上下关系做到排序
+function uiSorter:SortTagIndexSetter()
+
+end
+
+-- 设置带有3D模型UI的SortTag，带3d模型的ui需要排序设置
+function uiSorter:SortTag3DSetter()
+
+end
+
+-- 添加打开面板时调用，会重排UI
+function uiSorter:AddPanel(uiPanel)
+
+end
+
+--  移除关闭面板时调用，会重排UI
+function uiSorter:RemovePanel(uiPanel)
+
+end
+
 return uiSorter
