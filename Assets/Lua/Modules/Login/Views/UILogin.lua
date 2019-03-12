7a36430d476e3dca6bf49a52a3ad43eefe9c0e32
马三小伙儿 <1790617178@qq.com>
2019-03-12 21:14:26 +0800

    为Login界面添加层级

--- Assets/Lua/Modules/Login/Views/UILogin.lua
+++ Assets/Lua/Modules/Login/Views/UILogin.lua
@@ -13,6 +13,7 @@ end
 -- virtual 子类可以初始化一些变量,ResId要在这里赋值
 function UILogin:InitParam()
     self.ResId = 100
+    self.uiDepthLayer = ECEnumType.UIDepth.NORMAL
 end
 
 -- override UI面板创建结束后调用，可以在这里获取gameObject和component等操作
